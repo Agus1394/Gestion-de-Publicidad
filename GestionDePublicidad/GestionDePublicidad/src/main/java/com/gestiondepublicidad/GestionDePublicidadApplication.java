00d716b33be6623d2acf8383bec6673da0c2415e
Agustina <maldonadoale1394@gmail.com>
2022-11-09 20:41:39 -0300

    inicio de proyecto en rama main

---
+++ GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/GestionDePublicidadApplication.java
@@ -0,0 +1,13 @@
+package com.gestiondepublicidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePublicidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDePublicidadApplication.class, args);
+	}
+
+}
