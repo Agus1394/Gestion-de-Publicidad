ba75462cfbf80f28fef8e3884f0c4abd722c9c07
Ezequiel Balasch <ezebalasch@gmail.com>
2022-11-17 21:52:29 -0300

    Terminando retoques en Calendario Servicio

--- GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/servicios/CalendarioServicio.java
+++ GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/servicios/CalendarioServicio.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.gestiondepublicidad.repositorios.CalendarioRepositorio;
 import org.springframework.transaction.annotation.Transactional;
 import com.gestiondepublicidad.excepciones.MiException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,39 +16,83 @@ public class CalendarioServicio {
     @Autowired
     private CalendarioRepositorio calendarioRepositorio;
     
+    //CREAR
     @Transactional
-    public Calendario subirCalendario(String idCalendario, 
-            String descripcionCalendario, String eventoCalendario) throws MiException {
+    public void registrar(String id, String descripcion,
+            String evento) throws MiException {
         
-        Calendario nuevoCalendario = new Calendario();
+        validar(id, descripcion, evento);
         
-        nuevoCalendario.setId_calendario(idCalendario);
-        nuevoCalendario.setDescripcion(descripcionCalendario);
-        nuevoCalendario.setEvento(eventoCalendario);
+        Calendario calendario = new Calendario();
         
-        return calendarioRepositorio.save(nuevoCalendario);
+        calendario.setId_calendario(id);
+        calendario.setDescripcion(descripcion);
+        calendario.setEvento(evento);
+        
+        calendarioRepositorio.save(calendario);
     }
     
+    //ACTUALIZAR
     @Transactional
-    public void modificarCalendario(String idCalendario, String descripcionCalendario,
-            String eventoCalendario) throws MiException {
+    public void modificarCalendario(String id, String descripcion,
+            String evento) throws MiException {
         
-        Optional<Calendario> respuesta = calendarioRepositorio.findById(idCalendario);
+        validar(id, descripcion, evento);
+        
+        Optional<Calendario> respuesta = calendarioRepositorio.findById(id);
         
         if (respuesta.isPresent()) {
             Calendario calendarioModif = new Calendario();
-            calendarioModif.setId_calendario(idCalendario);
-            calendarioModif.setDescripcion(descripcionCalendario);
-            calendarioModif.setEvento(eventoCalendario);
+            calendarioModif.setId_calendario(id);
+            calendarioModif.setDescripcion(descripcion);
+            calendarioModif.setEvento(evento);
             
             calendarioRepositorio.save(calendarioModif);
         }
     }
     
+    //LISTAR-QUERYS
+    public Calendario getOne(String id){
+        return calendarioRepositorio.getOne(id);
+    }
+    
+    public List<Calendario> listarTodos(){
+        List<Calendario> calendarios = new ArrayList<>();
+        return calendarios = calendarioRepositorio.findAll();
+    }
+    
+    public List<Calendario> BuscarEvento(String evento){
+        List<Calendario> calendarios = new ArrayList<>();
+        return calendarios = calendarioRepositorio.buscarEvento(evento);
+    }
+    
+    public List<Calendario> BuscarDescripcion(String descripcion){
+        List<Calendario> calendarios = new ArrayList<>();
+        return calendarios = calendarioRepositorio.buscarDescripcion(descripcion);
+    }
+    
+    //ELIMINAR
     @Transactional
     public void eliminarCalendario (String idCalendario){
         calendarioRepositorio.deleteById(idCalendario);
     }
     
-    //me faltan las queris y alguna validacion !!
+    //VALIDAR
+        private void validar(String id, String descripcion, String evento)
+                throws MiException {
+
+        if (id.isEmpty() || id == null) {
+            throw new MiException("El id del Calendario no puede estar vacío");
+        }
+
+        if (descripcion.isEmpty() || descripcion == null) {
+            throw new MiException("La descripcion del calendario no puede estar vacía");
+        }
+
+        if (evento.isEmpty() || evento == null) {
+            throw new MiException("El evento del calendario no puede estar vacía");
+        }
+
+    }
+    
 }
