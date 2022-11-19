74e7eb2c3e55b3c90e3066581300428ab3125a57
Agustina <maldonadoale1394@gmail.com>
2022-11-19 18:33:22 -0300

    agrego cambios en clases

--- GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/servicios/ListaDeTareaServicio.java
+++ GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/servicios/ListaDeTareaServicio.java
@@ -1,5 +1,18 @@
 package com.gestiondepublicidad.servicios;
 
+import com.gestiondepublicidad.repositorios.ListaDeTareaRepositorio;
+import com.gestiondepublicidad.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ListaDeTareaServicio {
+    
+    @Autowired
+    private ListaDeTareaRepositorio listaDeTareaRepositorio;
+    
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+    
     
 }
