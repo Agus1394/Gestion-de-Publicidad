0df8147b9cc670a297915ab60e64646613f55555
Nicolas Casado <nicolascasado@gmail.com>
2022-11-28 21:18:50 -0300

    proyecto actualizado

--- GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/controladores/PortalControlador.java
+++ GestionDePublicidad/GestionDePublicidad/src/main/java/com/gestiondepublicidad/controladores/PortalControlador.java
@@ -2,6 +2,8 @@ package com.gestiondepublicidad.controladores;
 
 import com.gestiondepublicidad.entidades.Usuario;
 import com.gestiondepublicidad.excepciones.MiException;
+import com.gestiondepublicidad.repositorios.UsuarioRepositorio;
+import com.gestiondepublicidad.servicios.ProyectoServicio;
 import com.gestiondepublicidad.servicios.UsuarioServicio;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,12 @@ public class PortalControlador {
 
     @Autowired
     UsuarioServicio usuarioServicio;
+
+    @Autowired
+    ProyectoServicio proyectoServicio;
+
+    @Autowired
+    UsuarioRepositorio usuarioRepositorio;
 
     @GetMapping("/")
     public String index() {
@@ -65,13 +73,14 @@ public class PortalControlador {
     public String inicio(ModelMap modelo, HttpSession session) {
 
         Usuario logueado = (Usuario) session.getAttribute("usuariosession");
-
+        Usuario usuario = usuarioServicio.usuariosPorEmail(logueado.getEmail());
         if (logueado.getRol().toString().equals("ADMIN")) {
-
             return "dashboard.html";
         } else if (logueado.getRol().toString().equals("CLIENTE")) {
-            return "redirect:/cliente/dashboard";
+            modelo.addAttribute("proyectos", usuario.getProyecto());
+            return "indexCliente.html";
         } else if (logueado.getRol().toString().equals("USER")){
+            modelo.addAttribute("proyectos", usuario.getProyecto());
             return "indexTrabajador.html";
         }
             return "index.html";
