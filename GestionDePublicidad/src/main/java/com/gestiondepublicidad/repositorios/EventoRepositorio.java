86942bd4a7b715793dcb803e69e2380193748d6d
Ezequiel Balasch <ezebalasch@gmail.com>
2022-12-01 19:02:25 -0300

    Evento version estable

--- GestionDePublicidad/src/main/java/com/gestiondepublicidad/repositorios/EventoRepositorio.java
+++ GestionDePublicidad/src/main/java/com/gestiondepublicidad/repositorios/EventoRepositorio.java
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Repository;
 public interface EventoRepositorio extends JpaRepository<Evento, String> {
 
     // BUSQUEDA DE EVENTO POR SU NOMBRE
-    @Query("SELECT e FROM Evento e  WHERE e.nombre LIKE %:nombre% AND u.tipo LIKE %:tipo%")
+    @Query("SELECT e FROM Evento e  WHERE e.nombre LIKE %:nombre% AND e.tipo LIKE %:tipo%")
     List<Evento> buscarPorNombre(@Param("nombre") String nombre, @Param("tipo") String tipo);
 
     // BUSQUEDA DE EVENTO POR SU DESCRIPCION
-    @Query("SELECT e FROM Evento e  WHERE e.descripcion LIKE %:descripcion% AND u.tipo LIKE %:tipo%")
+    @Query("SELECT e FROM Evento e  WHERE e.descripcion LIKE %:descripcion% AND e.tipo LIKE %:tipo%")
     List<Evento> buscarPorDescripcion(@Param("descripcion") String descripcion, @Param("tipo") String tipo);
 
     //FILTRAR EVENTO POR FECHA (DUDOSO)
-    @Query("SELECT e FROM Evento e  WHERE e.fecha = :fecha AND u.tipo LIKE %:tipo%")
+    @Query("SELECT e FROM Evento e  WHERE e.fecha = :fecha AND e.tipo LIKE %:tipo%")
     List<Evento> buscarPorFecha(@Param("fecha") String fecha, @Param("tipo") String tipo);
 
     //FILTRAR EVENTO POR TIPO EVENTOD
-    @Query("SELECT e FROM Evento e  WHERE u.tipo LIKE %:tipo%")
+    @Query("SELECT e FROM Evento e  WHERE e.tipo LIKE %:tipo%")
     List<Evento> buscarPorTipoEvento(@Param("tipo") String tipo);
 
 }
