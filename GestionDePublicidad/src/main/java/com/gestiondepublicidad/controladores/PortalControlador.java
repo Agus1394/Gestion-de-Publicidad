fdcd4582e48b6b3a5628cfae3bb733ac9d87b470
Ezequiel Balasch <ezebalasch@gmail.com>
2022-12-05 11:47:57 -0300

    Ruta Cliente completa (testear con proyecto)

--- GestionDePublicidad/src/main/java/com/gestiondepublicidad/controladores/PortalControlador.java
+++ GestionDePublicidad/src/main/java/com/gestiondepublicidad/controladores/PortalControlador.java
@@ -1,10 +1,13 @@
 package com.gestiondepublicidad.controladores;
 
+import com.gestiondepublicidad.entidades.Proyecto;
 import com.gestiondepublicidad.entidades.Usuario;
 import com.gestiondepublicidad.excepciones.MiException;
 import com.gestiondepublicidad.repositorios.UsuarioRepositorio;
 import com.gestiondepublicidad.servicios.ProyectoServicio;
 import com.gestiondepublicidad.servicios.UsuarioServicio;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -74,32 +77,104 @@ public class PortalControlador {
         Usuario logueado = (Usuario) session.getAttribute("usuariosession");
         Usuario usuario = usuarioServicio.usuariosPorEmail(logueado.getEmail());
         modelo.put("usuario", usuario);
-        if(usuario.getRol().toString().equals("ADMIN")) {
+        if (logueado.getRol().toString().equals("ADMIN")) {
             return "dashboard.html";
-        } else if (usuario.getRol().toString().equals("USER")) {
+        } else if (logueado.getRol().toString().equals("USER")) {
             modelo.addAttribute("proyectos", proyectoServicio.buscarPorUsuario(usuario.getId_usuario()));
             return "indexCliente.html";
-        } else if (usuario.getRol().toString().equals("TRABAJADOR")){
+        } else if (logueado.getRol().toString().equals("TRABAJADOR")) {
             modelo.addAttribute("proyectos", proyectoServicio.buscarPorUsuario(usuario.getId_usuario()));
             return "indexTrabajador.html";
         }
-            return "index.html";
+        return "index.html";
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_TRABAJADOR', 'ROLE_ADMIN')")
+    //----------------------------------------PROYECTOS--------------------------------------------
+    //FILTRAR POR NOMBRE
+    @GetMapping("/cliente/tablaProyectos")
+    public String listarProyectos(ModelMap modelo) {
+        List<Proyecto> proyectos = proyectoServicio.listarTodos();
+        modelo.addAttribute("proyectos", proyectos);
+        return "indexCliente.html";
+    }
+
+    //filtrar por nombre
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
+    @PostMapping("/cliente/tablaProyectos/nombre")
+    public String filtrarPorNombre(@RequestParam String nombre, ModelMap modelo) {
+        List<Proyecto> proyectos = new ArrayList<Proyecto>();
+        if (nombre.isEmpty() || nombre == null) {
+            proyectos = proyectoServicio.listarTodos();
+        } else {
+            proyectos = proyectoServicio.buscarPorNombre(nombre.toUpperCase());
+        }
+        modelo.addAttribute("proyectos", proyectos);
+        return "redirect:/inicio";
+    }
+
+    //FILTRAR POR ESTADO DEL PROYECTO
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
+    @PostMapping("/cliente/tablaProyectos/estado")
+    public String filtrarPorEstadoProyecto(@RequestParam String estado, ModelMap modelo)
+            throws MiException {
+
+        List<Proyecto> proyectos = new ArrayList<Proyecto>();
+
+        if (estado.isEmpty() || estado == null) {
+            proyectos = proyectoServicio.listarTodos();
+        } else {
+            proyectos = proyectoServicio.filtrarProyectoPorEstado(estado);
+        }
+        modelo.addAttribute("proyectos", proyectos);
+        return "redirect:/inicio";
+    }
+
+    //FILTRAR POR FECHA DE INICIO
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
+    @PostMapping("/cliente/tablaProyectos/fechaInicio")
+    public String ordenarProyectosPorFechaInicio(@RequestParam String fechaInicio, ModelMap modelo) {
+        List<Proyecto> proyectos = new ArrayList<Proyecto>();
+
+        if (fechaInicio.isEmpty() || fechaInicio == null) {
+            proyectos = proyectoServicio.listarTodos();
+        } else {
+            proyectos = proyectoServicio.ordenarProyectosPorFechaInicio(fechaInicio);
+        }
+
+        modelo.addAttribute("proyectos", proyectos);
+        return "redirect:/inicio";
+    }
+
+    //FILTRAR POR FECHA DE FIN
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
+    @PostMapping("/cliente/tablaProyectos/fechaFin")
+    public String ordenarProyectosPorFechaFin(@RequestParam String fechaFin, ModelMap modelo) {
+        List<Proyecto> proyectos = new ArrayList<Proyecto>();
+
+        if (fechaFin.isEmpty() || fechaFin == null) {
+            proyectos = proyectoServicio.listarTodos();
+        } else {
+            proyectos = proyectoServicio.ordenarProyectosPorFechaFin(fechaFin);
+        }
+
+        modelo.addAttribute("proyectos", proyectos);
+        return "redirect:/inicio";
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @GetMapping("/perfil")
     public String perfil(ModelMap modelo, HttpSession session) {
         Usuario usuario = (Usuario) session.getAttribute("usuariosession");
 
-       // Usuario usuario1 = usuarioServicio.usuariosPorEmail(usuario.getEmail());
+        // Usuario usuario1 = usuarioServicio.usuariosPorEmail(usuario.getEmail());
         modelo.put("usuario", usuario);
         return "perfil.html";
     }
 
     //ACTUALIZAR
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_TRABAJADOR', 'ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @PostMapping("/perfil/{id}")
-    
+
     //REVISAR FOTO Y RETURN DEL TRY
     public String actualizar(@RequestParam MultipartFile archivo, @PathVariable String id,
             @RequestParam String nombre, @RequestParam String email,
